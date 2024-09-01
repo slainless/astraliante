f195a571328c73034a0c2c5fb6e4ccdd6952b19d
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-09-01 20:21:35 +0800

    added nyxbui components

---
+++ app/components/ui/skeleton.tsx
@@ -0,0 +1,15 @@
+import { ny } from '~/lib/utils'
+
+function Skeleton({
+   className,
+   ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+   return (
+      <div
+         className={ny('bg-muted animate-pulse rounded-md', className)}
+         {...props}
+      />
+   )
+}
+
+export { Skeleton }
