ad7db8f02906be1d9a7d8b063540952ff769965f
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-09-01 20:09:06 +0800

    initiate tailwind & nyxbui

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,13 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import type { LinksFunction } from '@remix-run/node';
+
+import styles from "./tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
 
 export default function App() {
   return (
