55a2fb935df90f68877749f8db292c3f0af9d175
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-09-09 08:59:25 +0800

    chore(sb): render jotai global store

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from "@storybook/react"
 import { TooltipProvider } from "@radix-ui/react-tooltip"
 import "~/tailwind.css"
 import { createElement } from "react"
+import { GlobalStore } from "#lib/jotai"
 
 const preview: Preview = {
   parameters: {
@@ -15,7 +16,12 @@ const preview: Preview = {
       stylePreview: true,
     },
   },
-  decorators: [(Story) => createElement(TooltipProvider, null, [Story()])],
+  decorators: [
+    (Story) =>
+      createElement(TooltipProvider, null, [
+        createElement(GlobalStore, null, [Story()]),
+      ]),
+  ],
 }
 
 export default preview
