6944110794d57abff365b6f5d2699f65796c3e73
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-09-09 15:22:34 +0800

    feat(lib): added Object utils

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function ny(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+namespace __Object {
+  export function partition<T extends Record<any, any>, K extends keyof T>(
+    obj: T,
+    keys: readonly K[]
+  ): [Pick<T, K>, Omit<T, K>] {
+    const picked = {} as any
+    const omitted = Object.assign({}, obj)
+
+    for (const key of keys) {
+      picked[key] = obj[key]
+      delete omitted[key]
+    }
+
+    return [picked, omitted]
+  }
+}
+
+export { __Object as Object }
